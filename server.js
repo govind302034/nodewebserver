55ea2c670393862394bd700bfa70af299f912043
Govind Maheshwari <govind302034@gmail.com>
2019-03-10 14:59:10 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express=require('express');
+
+const fs=require('fs');
+const hbs=require('hbs');
+
+const app=express();
+
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname + '/views/partial')
+
+
+
+
+app.use((req,res,next)=>{
+	let time=new Date().toString();
+	let log=`${time}: ${req.method} ${req.url}`;
+	fs.appendFile('server.log',log + '\n',(err)=>{
+		if(err) {throw err;
+		console.log('file is not complete');}
+	})
+	console.log(log);
+	next();
+})
+
+app.use((req,res,next)=>{
+	res.render('maintain.hbs')
+})
+
+app.use(express.static(__dirname + '/public'));
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		title:'about page',
+		currentyear: new Date().getFullYear()
+	});
+});
+
+app.get('/',(req,res)=>{
+	res.render('home.hbs',{
+		title:'home page',
+		currentyear:new Date().getFullYear()
+	});
+});
+
+
+
+app.listen(3000,()=>{
+	console.log('port is running');
+});
